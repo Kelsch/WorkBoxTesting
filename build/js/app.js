dfe9c79189927b27b6c8500bc5e7646bfe4619c4
Kelsch <dalankelsch@hotmail.com>
2020-04-01 10:25:16 -0600

    Moved location of api call functions

--- build/js/app.js
+++ build/js/app.js
@@ -26,40 +26,40 @@ fetch('userTestJson.json')
         });
     });
 
-getNonWorkDays = () => {
-    fetch('https://pdwebapi-mf5.conveyor.cloud/api/installerAppData/getNonWorkDays')
-    .then(response => response.json())
-    .then(data => {
-        let days = data;
-        const currentCalendar = document.getElementById('calendar_dayContainer');
-        return days.map(function (day) {
-            const dateElement = currentCalendar.querySelector(`[id='${day.mm}-${day.dd}-${day.yy}']`);
-            if (dateElement !== null && !day.isWorkDay) {
-                dateElement.classList.add('calendar-nonWorkDay');
-            }
-        });
-    });
-}
+// getNonWorkDays = () => {
+//     fetch('https://pdwebapi-mf5.conveyor.cloud/api/installerAppData/getNonWorkDays')
+//     .then(response => response.json())
+//     .then(data => {
+//         let days = data;
+//         const currentCalendar = document.getElementById('calendar_dayContainer');
+//         return days.map(function (day) {
+//             const dateElement = currentCalendar.querySelector(`[id='${day.mm}-${day.dd}-${day.yy}']`);
+//             if (dateElement !== null && !day.isWorkDay) {
+//                 dateElement.classList.add('calendar-nonWorkDay');
+//             }
+//         });
+//     });
+// }
 
-let timesRunGetJobs = 0;
-getJobs = () => {
-    fetch('https://pdwebapi-mf5.conveyor.cloud/api/installerAppData/getInstallIndicators?businessId=2')
-    .then(response => response.json())
-    .then(data => {
-        if (timesRunGetJobs > 0) {
-            return;
-        }
-        timesRunGetJobs++;
-        let jobs = data;
-        const currentCalendar = document.getElementById('calendar_dayContainer');
-        return jobs.map(function (job) {
-            const jobInstallDate = new Date(job.installDate);
-            const installElement = currentCalendar.querySelector(`[id='indicatorContainer${jobInstallDate.getMonth() + 1}-${jobInstallDate.getDate()}-${jobInstallDate.getFullYear()}']`);
+// let timesRunGetJobs = 0;
+// getJobs = () => {
+//     fetch('https://pdwebapi-mf5.conveyor.cloud/api/installerAppData/getInstallIndicators?businessId=2')
+//     .then(response => response.json())
+//     .then(data => {
+//         if (timesRunGetJobs > 0) {
+//             return;
+//         }
+//         timesRunGetJobs++;
+//         let jobs = data;
+//         const currentCalendar = document.getElementById('calendar_dayContainer');
+//         return jobs.map(function (job) {
+//             const jobInstallDate = new Date(job.installDate);
+//             const installElement = currentCalendar.querySelector(`[id='indicatorContainer${jobInstallDate.getMonth() + 1}-${jobInstallDate.getDate()}-${jobInstallDate.getFullYear()}']`);
             
-            if (installElement !== null) {
-                installElement.innerHTML += '<div class="dateNumber-indicator"></div>';
-            }
-        });
-    });
-    timesRunGetJobs = 0;
-}
+//             if (installElement !== null) {
+//                 installElement.innerHTML += '<div class="dateNumber-indicator"></div>';
+//             }
+//         });
+//     });
+//     timesRunGetJobs = 0;
+// }
