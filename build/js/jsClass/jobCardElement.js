6131657694865b29202ad804ba8b27e373385c55
Kelsch <dalankelsch@hotmail.com>
2020-04-17 14:06:45 -0600

    Changed icon and margin

--- build/js/jsClass/jobCardElement.js
+++ build/js/jsClass/jobCardElement.js
@@ -245,7 +245,7 @@ class JobCard extends HTMLElement {
                         </span> -->
                     <span class="job-button layout-button mdc-button${installColor}" data-buttontype="layouts" title="layouts" jobid="${this.jobId}" onclick="jobLayouts('${this.jobId}')">
                         <div class="mdc-button__ripple"></div>
-                        <i class="material-icons">arrow_forward</i>
+                        <i class="material-icons">perm_media</i>
                     </span>
                 </div>
             </div>
